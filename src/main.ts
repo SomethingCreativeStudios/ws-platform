4aa99407f21b14114e34086d05d2063e47a434b7
SomethingCreativeStudios <eric-417@live.com>
2019-12-07 19:23:32 -0500

    Updating plateform

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,13 @@
 import Vue from "vue";
 import App from "./App.vue";
 import store from "./store";
+import BattleGround from 'ws-battleground';
+
+Vue.use(BattleGround);
 
 Vue.config.productionTip = false;
 
 new Vue({
   store,
-  render: h => h(App)
+  render: h => h(App),
 }).$mount("#app");
